51d7ebbf96309383acd185769e803d2f8c37117e
8888 <leecostello1@gmail.com>
2021-12-25 00:10:22 -0500

    Initial commit

---
+++ test/sprocket-service.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SprocketService from '../lib/sprocket-service-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/sprocket-service-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SprocketService.SprocketServiceStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
