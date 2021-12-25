1594cb1f28ef35c615d406c4a58d9010184a28cf
8888 <leecostello1@gmail.com>
2021-12-25 00:17:24 -0500

    specify pipeline account for the pipeline stack

--- bin/sprocket-service.ts
+++ bin/sprocket-service.ts
@@ -15,7 +15,10 @@ new SprocketServiceStack(app, 'SprocketServiceStack', {
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+  // Pipeline account
+  env: { account: '293675922272', region: 'us-east-1' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
+
+app.synth();
